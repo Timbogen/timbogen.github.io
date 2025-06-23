5fbbcdc6c71f5dbeae3083dfe64dba9562286ffb
Tim Niederer <tim.niederer@weichwarenprojekt.de>
2025-06-23 12:19:09 +0200

    feat: add the metadata for the root link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "@/app/styles/globals.scss";
+import { generateMetadata as dynamicMetadata } from "@/app/[locale]/layout";
+import { defaultLocale } from "@/i18n/config";
 
-/** Root layout */
+/**
+ * By default, generate the English metadata
+ */
+export const generateMetadata = () => dynamicMetadata({ params: Promise.resolve({ locale: defaultLocale }) });
+
+/**
+ * Root layout
+ */
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return children;
 }
